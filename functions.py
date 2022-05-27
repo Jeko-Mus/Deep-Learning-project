afe78731a7a441a941feaa0c13c0d60a774a6161
Jeko <57463439+Jeko-Mus@users.noreply.github.com>
2022-05-27 12:24:07 +0200

    updated

--- functions.py
+++ functions.py
@@ -18,6 +18,6 @@ def bag_of_words(tokenized_sentence, words):
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
         if w in sentence_words: 
-            bag[idx] = 1
+            bag[idx] = 1.0
 
     return bag
